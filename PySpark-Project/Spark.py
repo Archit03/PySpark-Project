121fe9bbf73c433e23a012bb5ba37021235d39fd
Archit03 <architsood99@hotmail.com>
2023-12-18 19:27:59 +0530

    New adds.

--- PySpark-Project/Spark.py
+++ PySpark-Project/Spark.py
@@ -1,7 +1,5 @@
 from pyspark.sql import SparkSession as sk
 from pyspark.sql.functions import col, avg, max, min
-from pyspark.ml.feature import VectorAssembler
-
 
 # Initialize Spark session
 spark = sk.builder \
@@ -9,10 +7,12 @@ spark = sk.builder \
     .getOrCreate()
 
 # Read the CSV file into a Pandas DataFrame
-df = spark.read.csv("E:\\Dev\PySpark-Project\\PySpark-Project\\brewery_data_complete_extended.csv", header=True, inferSchema=True)
+df = spark.read.csv("C:\\Users\\LENOVO\\Desktop\\New "
+                    "folder\\PySpark-Project\\PySpark-Project\\brewery_data_complete_extended.csv", header=True,
+                    inferSchema=True)
 
 # Show the PySpark DataFrame
-#df.describe().show()
+df.describe().show()
 
 # Average alcohol content across all batches
 average_alcohol_content = df.select(avg('Alcohol_Content')).collect()[0][0]
@@ -21,21 +21,21 @@ print(f"Average Alcohol Content: {average_alcohol_content:.2f}%")
 # Beer style with the highest alcohol content using a subquery
 max_alcohol_content = df.agg(max('Alcohol_Content').alias('max_alcohol_content')).collect()[0]['max_alcohol_content']
 max_alcohol_beer_style = df.filter(col('Alcohol_Content') == max_alcohol_content).select('Beer_Style').collect()[0][0]
-#print(f"Beer Style with the Highest Alcohol Content: {max_alcohol_beer_style}")
+print(f"Beer Style with the Highest Alcohol Content: {max_alcohol_beer_style}")
 
 # Location with the highest total sales
 max_sales_location = df.agg(max('Total_Sales').alias('max_sales')).join(df, col('max_sales') == col('Total_Sales')).select('Location').collect()[0][0]
-#print(f"Location with the Highest Total Sales: {max_sales_location}")
+print(f"Location with the Highest Total Sales: {max_sales_location}")
 
 # Batch with the highest quality score
 max_quality_batch_id = df.agg(max('Quality_Score').alias('max_quality_score')).join(df, col('max_quality_score') == col('Quality_Score')).select('Batch_ID').collect()[0][0]
-#print(f"Batch with the Highest Quality Score: {max_quality_batch_id}")
+print(f"Batch with the Highest Quality Score: {max_quality_batch_id}")
 
 # Batches with Alcohol Content greater than a certain threshold (e.g., 5%)
 high_alcohol_batches = df.filter(col('Alcohol_Content') > 5)
-#high_alcohol_batches.show()
+high_alcohol_batches.show()
 
-# More advanced analytics can involve joining with other DataFrames, grouping, and aggregating data based on specific criteria.
-#Finding average sales per location:
+# More advanced analytics can involve joining with other DataFrames, grouping, and aggregating data based on specific
+#  criteria. Finding average sales per location:
 avg_sales_per_location = df.groupBy('Location').agg(avg('Total_Sales').alias('Average_Sales'))
-#avg_sales_per_location.show()
+avg_sales_per_location.show()
