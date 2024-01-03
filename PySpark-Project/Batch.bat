2fb9f21dae579cf2c24d1cb928e5d7e22db42385
Archit Sood <architsood99@hotmail.com>
2024-01-03 22:03:36 +0530

    New

--- PySpark-Project/Batch.bat
+++ PySpark-Project/Batch.bat
@@ -1,10 +1,8 @@
-#!/bin/bash
-
 # Create virtual environment
 python -m venv venv
 
 # Activate virtual environment
-source venv/bin/activate
+venv\Scripts\activate
 
 # Install dependencies
 pip install -r requirements.txt
